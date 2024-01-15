0b5a053422efc54a585b1d80b6952b85ddfa78d3
xg0n <letandat.dc@gmail.com>
2024-01-15 11:39:59 +0700

    Fix formatter for bytes/strings.Reader

--- log/log.go
+++ log/log.go
@@ -8,7 +8,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"reflect"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -50,39 +49,41 @@ func setup(output, prefix string, size int) {
 // strip down empty slices, and don't print the internals from buffers.
 func formatter(i interface{}, size int) (s string) {
 	// don't show the internal state of buffers
-	switch i := i.(type) {
+	switch v := i.(type) {
 	case *bufio.Reader:
 		s = "&bufio.Reader{}"
 	case *bufio.Writer:
 		s = "&bufio.Writer{}"
 	case *bytes.Buffer:
-		s = fmt.Sprintf("&bytes.Buffer{%q}", i.String())
+		s = fmt.Sprintf("&bytes.Buffer{%q}", v.String())
 	case *bytes.Reader:
-		v := reflect.ValueOf(i)
-		// TODO: should probably iterate to find the slice in case the name changes
-		if b, ok := v.FieldByName("s").Interface().([]byte); ok {
-			if len(b) > size {
-				b = b[:size]
-			}
-			s = fmt.Sprintf("&bytes.Reader{%q}", b)
+		buf := make([]byte, min(size, v.Len()))
+		_, err := io.ReadFull(v, buf)
+		if err != nil {
+			s = "&bytes.Reader{unknown}"
+		} else {
+			s = string(buf)
 		}
 	case *strings.Reader:
-		v := reflect.ValueOf(i)
-		if f, ok := v.FieldByName("s").Interface().(string); ok {
-			s = fmt.Sprintf("&strings.Reader{%q}", f)
+		buf := make([]byte, min(size, v.Len()))
+		_, err := io.ReadFull(v, buf)
+		if err != nil {
+			s = "&strings.Reader{unknown}"
+		} else {
+			s = string(buf)
 		}
 	case []byte:
 		// bytes slices are often empty, so trim them down
-		b := bytes.TrimLeft(i, "\x00")
+		b := bytes.TrimLeft(v, "\x00")
 		if len(b) == 0 {
 			s = "[]byte{0...}"
-		} else if utf8.Valid(i) {
-			s = fmt.Sprintf("[]byte{%q}", i)
+		} else if utf8.Valid(v) {
+			s = fmt.Sprintf("[]byte{%q}", v)
 		} else {
-			s = fmt.Sprintf("%#v", i)
+			s = fmt.Sprintf("%#v", v)
 		}
 	case string:
-		s = fmt.Sprintf("%q", i)
+		s = fmt.Sprintf("%q", v)
 	}
 
 	if s == "" {
